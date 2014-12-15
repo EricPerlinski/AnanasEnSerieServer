8f44798379b11d484fea330cda6bdd84bc9d0158
Valentin GIANNINI <valentin.giannini@telecomnancy.net>
2014-12-15 11:55:06 +0100

    init slim index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-ANANAS EN SERIE
+<?php
+
+require 'vendor/slim/slim/Slim/Slim.php';
+\Slim\Slim::registerAutoloader();
+
+
+$app = new \Slim\Slim();
+
+$app->get('/', function () {
+    echo "Ananas en serie";
+});
+
+$app->run();
+

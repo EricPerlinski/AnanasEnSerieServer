921c92c417db592b791e3fe3733430c0c069cdd4
Valentin GIANNINI <valentin.giannini@telecomnancy.net>
2014-12-16 11:50:41 +0100

    ajout des vues dans les routes pour vote et resultat

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 use Bridge\Doctrine\EntityManager as EM;
 
-use App\Entity\QRCode as QR;
+use App\Entity\QRCode as QRCode;
 
 require 'vendor/autoload.php';
 
@@ -13,7 +13,7 @@ define('ROOT', __DIR__);
 
 \Slim\Slim::registerAutoloader();
 
-$loader = new Twig_Loader_Filesystem('view');
+$loader = new Twig_Loader_Filesystem('src/App/view');
 Twig_Autoloader::register();
 $twig = new Twig_Environment($loader, array());
 
@@ -23,41 +23,78 @@ $app = new \Slim\Slim(array(
 $app->config = require(__DIR__ . '/app/config/config.php');
 
 $em = new EM($app);
-$em = $em->getEntityManager();
+$em  = $em->getEntityManager();
 
 
 $app->get('/', function () use($app,$twig){
 	echo $twig->render('index.php');
 })->name('home');
 
-$app->post('/admin/add', function () use($app,$twig,$em){
+
+
+
+$app->post('/api/admin/add', function () use($app,$twig,$em){
     //traitement des params POST
-    $titre;
+	$titre;
 	if(isset($_POST['titre'])){
 		$titre = $_POST['titre'];
-    }else{
-    	$app->notFound();
-    }
+	}else{
+		$app->notFound();
+	}
 
-	$qr = new QR();
+	$qr = new QRCode();
 	$qr->setTitle($titre);
+	$qr->setPath(rand(1,1000));
+	$qr->setPathAdmin(rand(1,1000));
 	
 	$em->persist($qr);
 	$em->flush();
 
 	
-	//envoie du resultat
+	//RENDER
 	$id=$qr->getId();
-    echo "{id:$id}";
-    $app->response->setStatus(200);
+	$path=$qr->getPath();
+	$pathAdmin=$qr->getPathAdmin();
+	echo "[{\"path\":\"$path\",\"pathAdmin\":\"$pathAdmin\"}]";
+	$app->response->setStatus(200);
+
 })->name('add');
 
 
 
-$app->get('/client/view/:id', function ($id) use($app,$twig){
-	echo $twig->render('view.php',array('nom'=>"TODO"));
+
+$app->get('api/admin/get/:pathAdmin', function ($pathAdmin) use($app,$twig,$em){
+
+	$qr = $em->getRepository("App\Entity\QRCode")->findBy(array('pathAdmin' => $pathAdmin));
+
+	//RENDER
+	echo $twig->render('resultat.php',array('nom'=> $qr->getTitle(), 'nb' => $qr->getCounter() ));	
 	$app->response->setStatus(200);
-})->name('view')->conditions(['id' => '[0-9]+']);
+
+})->name('viewAdmin')->conditions(['pathAdmin' => '[0-9a-zA-Z]+']);
+
+
+
+
+$app->get('/api/get/:path', function ($path) use($app,$twig,$em){
+
+	$qr = $em->getRepository("App\Entity\QRCode")->findBy(array('path' => $path));
+	if(count($qr)==0){
+		$app->notFound();
+	}
+	$qr = $qr[0];
+	$qr->increment();
+	$em->persist($qr);
+	$em->flush();
+
+	//Render
+	$titre = $qr->getTitle();
+	
+
+	echo $twig->render('vote.php',array('nom' => $titre));
+	$app->response->setStatus(200);
+
+})->name('view')->conditions(['path' => '[0-9a-zA-Z]+']);
 
 $app->run();
 

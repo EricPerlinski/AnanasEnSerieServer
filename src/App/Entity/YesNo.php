90d2840f1291b120f403784a007eac5c2cea4c95
Nicolas Viard <nicolas.viard@telecomnancy.net>
2014-12-17 17:49:11 +0100

    hotfix bdd

--- src/App/Entity/YesNo.php
+++ src/App/Entity/YesNo.php
@@ -15,6 +15,22 @@ class YesNo extends QRCode
     protected $noPrefix = "no";
 
     /**
+     * @var integer
+     *
+     * @Column(name="counterno", type="integer")
+     */
+    private $counterNo;
+
+    /**
+     * @ManyToMany(targetEntity="ClickLog")
+     * @JoinTable(name="no_clicklogs",
+     *      joinColumns={@JoinColumn(name="yesno_id", referencedColumnName="id", onDelete="CASCADE")},
+     *      inverseJoinColumns={@JoinColumn(name="clicklog_id", referencedColumnName="id", unique=true)}
+     *      )
+     **/
+    private $clickLogNo;
+
+    /**
      * Get noPath
      *
      * @return string 
@@ -22,5 +38,68 @@ class YesNo extends QRCode
     public function getNoPath()
     {
         return $this->noPrefix . "/" . $this->path;
+    }
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->clickLogNo = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Set counterNo
+     *
+     * @param integer $counterNo
+     * @return YesNo
+     */
+    public function setCounterNo($counterNo)
+    {
+        $this->counterNo = $counterNo;
+
+        return $this;
+    }
+
+    /**
+     * Get counterNo
+     *
+     * @return integer 
+     */
+    public function getCounterNo()
+    {
+        return $this->counterNo;
+    }
+
+    /**
+     * Add clickLogNo
+     *
+     * @param \App\Entity\ClickLog $clickLogNo
+     * @return YesNo
+     */
+    public function addClickLogNo(\App\Entity\ClickLog $clickLogNo)
+    {
+        $this->clickLogNo[] = $clickLogNo;
+
+        return $this;
+    }
+
+    /**
+     * Remove clickLogNo
+     *
+     * @param \App\Entity\ClickLog $clickLogNo
+     */
+    public function removeClickLogNo(\App\Entity\ClickLog $clickLogNo)
+    {
+        $this->clickLogNo->removeElement($clickLogNo);
+    }
+
+    /**
+     * Get clickLogNo
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getClickLogNo()
+    {
+        return $this->clickLogNo;
     }
 }

7a36b60ffe1d4deeceaaa7cde6ee91523a4efa83
Nicolas Viard <nicolas.viard@telecomnancy.net>
2014-12-17 16:58:07 +0100

    mise a jour diagramme bdd

--- src/App/Entity/YesNo.php
+++ src/App/Entity/YesNo.php
@@ -15,33 +15,12 @@ class YesNo extends QRCode
     protected $noPrefix = "no";
 
     /**
-     * @var string
-     *
-     * @Column(name="nopath", type="string", length=255)
-     */
-    private $noPath;
-
-
-    /**
-     * Set noPath
-     *
-     * @param string $noPath
-     * @return YesNo
-     */
-    private function setNoPath($noPath)
-    {
-        $this->noPath = $noPath;
-
-        return $this;
-    }
-
-    /**
      * Get noPath
      *
      * @return string 
      */
     public function getNoPath()
     {
-        return $this->noPrefix . "/" . $this->noPath;
+        return $this->noPrefix . "/" . $this->path;
     }
 }

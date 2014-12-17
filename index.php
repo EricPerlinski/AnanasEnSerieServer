91e84540c1fa15cec5c42680e16205d316a3a19a
Valentin GIANNINI <valentin.giannini@telecomnancy.net>
2014-12-17 17:23:29 +0100

    ajout des redirections

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ use Bridge\Doctrine\EntityManager as EM;
 
 use App\Entity\QRCode as QRCode;
 use App\Entity\Like as Like;
+use App\Entity\Redirect as Redirect;
 
 require 'vendor/autoload.php';
 
@@ -69,6 +70,29 @@ $app->get('/like/:path', function ($path) use($app,$twig,$em){
 
 })->name('like')->conditions(['path' => '[0-9a-zA-Z]+']);
 
+$app->get('/redirect/:path', function ($path) use($app,$twig,$em){
+
+	$vote = $app->getCookie("$path");
+	$qr = $em->getRepository("App\Entity\QRCode")->findBy(array('path' => $path));
+	if(count($qr)!=1){
+		$app->notFound();
+	}
+	$qr = $qr[0];
+
+	if($vote){
+		$qr->increment();
+	}
+
+	$em->persist($qr);
+	$em->flush();
+
+	$app->setCookie("$path",true);
+
+	$app->redirect($qr->getUrl());
+	$app->response->setStatus(200);
+	
+})->name('redirect')->conditions(['path' => '[0-9a-zA-Z]+']);
+
 
 
 
@@ -86,6 +110,22 @@ $app->get('/admin/get/like/:pathAdmin', function ($pathAdmin) use($app,$twig,$em
 	$app->response->setStatus(200);
 
 })->name('adminLike')->conditions(['pathAdmin' => '[0-9a-zA-Z]+']);
+
+
+$app->get('/admin/get/redirect/:pathAdmin', function ($pathAdmin) use($app,$twig,$em){
+
+	$qr = $em->getRepository("App\Entity\QRCode")->findBy(array('pathAdmin' => $pathAdmin));
+	if(count($qr)!=1){
+		$app->notFound();
+	}
+	$qr = $qr[0];
+	//RENDER
+	$title = $qr->getTitle();
+	$counter = $qr->getCounter();
+	echo $twig->render('redirect.php',array('name'=> $title, 'counter' => $counter ));	
+	$app->response->setStatus(200);
+
+})->name('adminRedirect')->conditions(['pathAdmin' => '[0-9a-zA-Z]+']);
 
 
 /*****************/
@@ -131,7 +171,39 @@ $app->post('/api/admin/add/like', function () use($app,$twig,$em){
 })->name('addLike');
 
 
+$app->post('/api/admin/add/redirect', function () use($app,$twig,$em){
+    //traitement des params POST
 
+	$json;
+	if(isset($_POST['objet'])){
+		$json = $_POST['objet'];
+	}else{ 
+		$app->notFound();
+	}
+
+	$obj = json_decode($json,true);
+	$qr = new Redirect();
+	$qr->setTitle($obj['title']);
+	$qr->setUrl($obj['url']);
+	$qr->setPath(rand(1,1000));
+	$qr->setPathAdmin(rand(1,1000));
+	$em->persist($qr);
+	$em->flush();
+
+	$qr->setPath(hash('crc32b', $qr->getCreationDate()->format('Y-m-d H:i:s') . $qr->getId()) . $qr->getId());
+	$qr->setPathAdmin(hash('crc32b', $qr->getCreationDate()->format('Y-m-d H:i:s') . 'admin' . $qr->getId()) . $qr->getId());
+	$em->persist($qr);
+	$em->flush();
+
+	
+	//RENDER
+	$id=$qr->getId();
+	$path=$qr->getPath();
+	$pathAdmin=$qr->getPathAdmin();
+	echo "[{\"path\":\"$path\",\"pathAdmin\":\"$pathAdmin\"}]";
+	$app->response->setStatus(200);
+
+})->name('addLike');
 
 $app->get('/api/admin/get/:pathAdmin', function ($pathAdmin) use($app,$twig,$em){
 
---
+++ src/App/view/redirect.php
@@ -0,0 +1,7 @@
+{% extends "template.php" %}
+
+{% block title %}Redirection pour {{ nom }} {% endblock %}
+
+{% block content %}
+	IL y a {{counter}} redirections pour {{name}}
+{% endblock %}

ab29f767a621af08c5c3ccc01590a1d627c5dd21
Nicolas Viard <nicolas.viard@telecomnancy.net>
2014-12-17 15:09:27 +0100

    ajout reponse openquestion

--- src/App/Entity/OpenQuestion.php
+++ src/App/Entity/OpenQuestion.php
@@ -12,4 +12,34 @@ use Doctrine\ORM\Mapping;
 class OpenQuestion extends Question
 {
 
+    /**
+     * @var string
+     *
+     * @Column(name="answer", type="text")
+     */
+    private $answer;
+
+
+    /**
+     * Set answer
+     *
+     * @param string $answer
+     * @return OpenQuestion
+     */
+    public function setAnswer($answer)
+    {
+        $this->answer = $answer;
+
+        return $this;
+    }
+
+    /**
+     * Get answer
+     *
+     * @return string 
+     */
+    public function getAnswer()
+    {
+        return $this->answer;
+    }
 }

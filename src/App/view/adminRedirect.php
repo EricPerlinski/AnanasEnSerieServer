745e99ddb052193820929c73b20432127f9441cf
Nicolas Viard <nicolas.viard@telecomnancy.net>
2014-12-18 21:03:51 +0100

    ajout vues stats

--- src/App/view/adminRedirect.php
+++ src/App/view/adminRedirect.php
@@ -4,15 +4,18 @@
 
 {% block content %}
 <div class="row">
-Il y a {{counter}} redirections pour {{name}}.
+	Il y a {{counter}} redirections pour {{name}}.
 </div>
 <div class="row">
-<form role="form" method="POST" action="{{ target }}">
-  <div class="form-group">
-    <label for="url">Destination</label>
-    <input type="url" class="form-control" id="url" name='url' placeholder="{{ url }}"/>
-  </div>
-  <button type="submit" class="btn btn-primary">Valider</button>
-</form>
+	{% include 'frequency.php' %}
+</div>
+<div class="row">
+	<form role="form" method="POST" action="{{ target }}">
+		<div class="form-group">
+			<label for="url">Destination</label>
+			<input type="url" class="form-control" id="url" name='url' placeholder="{{ url }}"/>
+		</div>
+		<button type="submit" class="btn btn-primary">Valider</button>
+	</form>
 </div>
 {% endblock %}

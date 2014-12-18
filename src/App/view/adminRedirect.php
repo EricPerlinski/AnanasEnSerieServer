0b02d7f2a5057c2ad157771055fee2c40d89da1f
Nicolas Viard <nicolas.viard@telecomnancy.net>
2014-12-18 16:09:31 +0100

    ajout formulaire redirections

--- src/App/view/adminRedirect.php
+++ src/App/view/adminRedirect.php
@@ -3,5 +3,12 @@
 {% block title %}Redirection pour {{ nom }} {% endblock %}
 
 {% block content %}
-	IL y a {{counter}} redirections pour {{name}}
+Il y a {{counter}} redirections pour {{name}}
+<form role="form">
+  <div class="form-group">
+    <label for="link">Lien</label>
+    <input type="url" class="form-control" id="link" placeholder="Lien">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
 {% endblock %}

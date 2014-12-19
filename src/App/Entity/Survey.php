165c9564edc349d1c12a76204b7209677cdeb113
Nicolas Viard <nicolas.viard@telecomnancy.net>
2014-12-19 01:05:51 +0100

    mise a jour bdd

--- src/App/Entity/Survey.php
+++ src/App/Entity/Survey.php
@@ -22,16 +22,6 @@ class Survey extends QRCode
      **/
     private $question;
 
-     /**
-     * @ManyToMany(targetEntity="Question")
-     * @JoinTable(name="survey_answers",
-     *      joinColumns={@JoinColumn(name="survey_id", referencedColumnName="id", onDelete="CASCADE")},
-     *      inverseJoinColumns={@JoinColumn(name="question_id", referencedColumnName="id")}
-     *      )
-     **/
-     private $answer;
-
-
     /**
      * Constructor
      */
@@ -76,42 +66,8 @@ class Survey extends QRCode
 
     public function jsonSerialize() {
         return array(
-            'question' =>  json_encode($this->getQuestion()),
-            'answer'     =>  json_encode($this->getAnswer()));
-    }
-
-
-    /**
-     * Add answer
-     *
-     * @param \App\Entity\Question $answer
-     * @return Survey
-     */
-    public function addAnswer(\App\Entity\Question $answer)
-    {
-        $this->answer[] = $answer;
-
-        return $this;
-    }
-
-    /**
-     * Remove answer
-     *
-     * @param \App\Entity\Question $answer
-     */
-    public function removeAnswer(\App\Entity\Question $answer)
-    {
-        $this->answer->removeElement($answer);
-    }
-
-    /**
-     * Get answer
-     *
-     * @return \Doctrine\Common\Collections\Collection 
-     */
-    public function getAnswer()
-    {
-        return $this->answer;
+            'question' =>  json_encode($this->getQuestion())
+        );
     }
 
     public function printSurvey(){
